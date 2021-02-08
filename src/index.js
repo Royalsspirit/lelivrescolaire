d8871f9d412c99ba617f2c96a79d3c85ae8b74ca
gmajkic <gmajkic@vente-privee.com>
2021-02-08 21:51:26 +0100

    feat(index): define redux management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducer from './reducers';
+import Routes from './routes';
+
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+const reduxDebugger = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+let store;
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(reducer, reduxDebugger, applyMiddleware(thunk));
+} else {
+  store = createStore(reducer, reduxDebugger, applyMiddleware(logger, thunk));
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import users from '../pages/users/Users.reducer';
+
+export default combineReducers({ users });
